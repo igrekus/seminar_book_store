92ca6a2f8442e4ed5926367726cd1bb10188393a
igrekus <upload234@mail.ru>
2022-10-27 01:32:21 +0300

    fix: use single quotes

--- src/book_shop.py
+++ src/book_shop.py
@@ -15,27 +15,27 @@ class BookShop:
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Д. Кнут, Искусство программирования" and item.name != "Скидочный купон на курс":
+            if item.name != 'Д. Кнут, Искусство программирования' and item.name != 'Скидочный купон на курс':
                 if item.quality > 0:
-                    if item.name != "Марк Лутц, Изучаем Python, 3й том":
+                    if item.name != 'Марк Лутц, Изучаем Python, 3й том':
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Скидочный купон на курс":
+                    if item.name == 'Скидочный купон на курс':
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Марк Лутц, Изучаем Python, 3й том":
+            if item.name != 'Марк Лутц, Изучаем Python, 3й том':
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Д. Кнут, Искусство программирования":
-                    if item.name != "Скидочный купон на курс":
+                if item.name != 'Д. Кнут, Искусство программирования':
+                    if item.name != 'Скидочный купон на курс':
                         if item.quality > 0:
-                            if item.name != "Марк Лутц, Изучаем Python, 3й том":
+                            if item.name != 'Марк Лутц, Изучаем Python, 3й том':
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
